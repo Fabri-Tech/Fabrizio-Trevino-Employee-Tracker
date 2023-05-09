7055c6b03fcedc414f8aca0a5ebabf2522ea1a30
fabri-tech <fabriziotrevino@gmail.com>
2023-05-09 16:42:21 -0600

    added case-break

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection(
   }
 );
 
-// Add options to the inquiries
+// Add options to the questions
 
 const options = [
   'See list of all departments',
@@ -25,3 +25,43 @@ const options = [
   'Modify an employee role',
   'exit',
 ];
+
+// Add questions for inquirer
+
+async function newQuestion() {
+  const { menu } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'menu',
+      message: 'what do you want to do?',
+      choices: options,
+    },
+  ]);
+
+  switch (menu) {
+    case 'See list of all departments':
+      await view('department');
+      break;
+    case 'See list of all job roles':
+      await view('role');
+      break;
+    case 'See list of all employees':
+      await view('employee');
+      break;
+    case 'Create a new department':
+      await addDepartment();
+      break;
+    case 'Create a new job role':
+      await addRole();
+      break;
+    case 'Create a new employee':
+      await addEmployee();
+      break;
+    case 'update an employee role':
+      await updateEmployee();
+      break;
+    case 'exit':
+      exit();
+      break;
+  }
+}
